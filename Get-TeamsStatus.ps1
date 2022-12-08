c6effca2f9b9bd8e2831bac21c5f0a9b7f70d3c4
damhau <damien.hauser@gmail.com>
2022-12-08 12:51:52 +0100

    Update Get-TeamsStatus.ps1

--- Get-TeamsStatus.ps1
+++ Get-TeamsStatus.ps1
@@ -21,7 +21,10 @@
     .\Get-TeamsStatus.ps1 -SetStatus "Offline"
 #>
 # Configuring parameter for interactive run
-Param($SetStatus)
+Param(
+    $SetStatus,
+    $debugEnabled = $false
+)
 
 # Set language variables below
 $lgAvailable = "Available"
@@ -37,224 +40,231 @@ $lgOffline = "Offline"
 $lgNotInACall = "Not in a call"
 $lgInACall = "In a call"
 
-# Set icons to use for call activity
-$iconInACall = "mdi:phone-in-talk-outline"
-$iconNotInACall = "mdi:phone-off"
-$iconMonitoring = "mdi:api"
+# Choose logging style, either eventlog or file
+$logType = "eventlog"
 
-# Set entities to post to
-$entityStatus = "sensor.teams_status"
-$entityStatusName = "Microsoft Teams status"
-$entityActivity = "sensor.teams_activity"
-$entityActivityName = "Microsoft Teams activity"
-$entityHeartbeat = "binary_sensor.teams_monitoring"
-$entityHeartbeatName = "Microsoft Teams monitoring"
-
-$Enable = 1
-
+# Config
+$teamsLogFile = "C:\Users\Damien\AppData\Roaming\Microsoft\Teams\logs.txt"
 $webhookUrl = "http://192.168.1.34:8000/webhook"
+$pollFrequency = 5
+$logFilePath = "Get-TeamsStatus.log"
+
+
+function Write-LogEvent {
+
+    param (
+        $logType,
+        $logMessage,
+        $enableStdout = $false
+    )
+
+    if ($logType -eq "eventlog") {
+        Write-EventLog -LogName "Application" -Source "TeamsStatus" -EventID 3001 -EntryType Information -Message $logMessage -Category 1 -RawData 10,20
+    }
+    if ($logType -eq "file") {
+        Add-Content $logFilePath "$(Get-Date) - Status: $logMessage"
+    }
+    if ($enableStdout -eq $true){
+        Write-Host "$(Get-Date) - $logMessage"
+    }
+}
+
+function Invoke-PythonWebHook {
+    param (
+        $color
+    )
+    $params = @{
+        "color"="$color";
+    }
+        
+    $params = $params | ConvertTo-Json
+
+    $result = try { 
+        (Invoke-RestMethod -Uri $webhookUrl -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($params)) -ContentType "application/json"  -ErrorAction Stop).BaseResponse
+    } catch [System.Net.WebException] { 
+        $result_error = $($_.Exception.Message)
+        Write-LogEvent -logtype $logType -enableStdout $true -logMessage "Teams Status webhook call failed result: $result_error"
+    } 
+    
+    if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Wehook result: $result" }
+
+}
+
 
 # Run the script when a parameter is used and stop when done
 If($null -ne $SetStatus){
-    Write-Host ("Setting Microsoft Teams status to "+$SetStatus+":")
+    Write-Host ("Setting Microsoft Teams light to " + $SetStatus )
     $params = @{
-     "state"="$SetStatus";
-     "attributes"= @{
-        "friendly_name"="$entityStatusName";
-        "icon"="mdi:microsoft-teams";
-        }
+     "color"="$SetStatus";
      }
 	 
     $params = $params | ConvertTo-Json
-    Invoke-RestMethod -Uri "$HAUrl/api/states/$entityStatus" -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($params)) -ContentType "application/json"
+    $result = Invoke-RestMethod -Uri $webhookUrl -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($params)) -ContentType "application/json" 
     break
 }
 
 # Start monitoring the Teams logfile when no parameter is used to run the script
-Write-EventLog -LogName "Application" -Source "TeamsStatus" -EventID 3001 -EntryType Information -Message "Teams Status started" -Category 1 -RawData 10,20
+Write-LogEvent -logtype $logType -enableStdout $true -logMessage "Teams Status started"
+Write-LogEvent -logtype $logType -enableStdout $true -logMessage "Processing Teams log file: $env:APPDATA\Microsoft\Teams\logs.txt"
 
+$Enable = 1
 DO {
-# Get Teams Logfile and last icon overlay status
-$TeamsStatus = Get-Content -Path $env:APPDATA"\Microsoft\Teams\logs.txt" -Tail 1000 | Select-String -Pattern `
-  'Setting the taskbar overlay icon -',`
-  'StatusIndicatorStateService: Added' | Select-Object -Last 1
+    # Get Teams Logfile and last icon overlay status   
+    $TeamsStatus = Get-Content -Path $teamsLogFile -Tail 1000 | Select-String -Pattern `
+    'Setting the taskbar overlay icon -',`
+    'StatusIndicatorStateService: Added' | Select-Object -Last 1
 
-# Get Teams Logfile and last app update deamon status
-$TeamsActivity = Get-Content -Path $env:APPDATA"\Microsoft\Teams\logs.txt" -Tail 1000 | Select-String -Pattern `
-  'Resuming daemon App updates',`
-  'Pausing daemon App updates',`
-  'SfB:TeamsNoCall',`
-  'SfB:TeamsPendingCall',`
-  'SfB:TeamsActiveCall',`
-  'name: desktop_call_state_change_send, isOngoing' | Select-Object -Last 1
+    # Get Teams Logfile and last app update deamon status
+    $TeamsActivity = Get-Content -Path $teamsLogFile -Tail 1000 | Select-String -Pattern `
+    'Resuming daemon App updates',`
+    'Pausing daemon App updates',`
+    'SfB:TeamsNoCall',`
+    'SfB:TeamsPendingCall',`
+    'SfB:TeamsActiveCall',`
+    'name: desktop_call_state_change_send, isOngoing' | Select-Object -Last 1
 
-# Get Teams application process
-$TeamsProcess = Get-Process -Name Teams -ErrorAction SilentlyContinue
+    # Get Teams application process
+    $TeamsProcess = Get-Process -Name Teams -ErrorAction SilentlyContinue
 
-# Check if Teams is running and start monitoring the log if it is
-If ($null -ne $TeamsProcess) {
-    If($TeamsStatus -eq $null){ }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgAvailable*" -or `
-        $TeamsStatus -like "*StatusIndicatorStateService: Added Available*" -or `
-        $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Available -> NewActivity*") {
-        $Status = $lgAvailable
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
+    # Check if Teams is running and start monitoring the log if it is
+    If ($null -ne $TeamsProcess) {
+        If($TeamsStatus -eq $null){ }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgAvailable*" -or `
+            $TeamsStatus -like "*StatusIndicatorStateService: Added Available*" -or `
+            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Available -> NewActivity*") {
+            $Status = $lgAvailable
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgBusy*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added Busy*" -or `
+                $TeamsStatus -like "*Setting the taskbar overlay icon - $lgOnThePhone*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added OnThePhone*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Busy -> NewActivity*") {
+            $Status = $lgBusy
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status"}
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgAway*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added Away*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Away -> NewActivity*") {
+            $Status = $lgAway
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgBeRightBack*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added BeRightBack*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: BeRightBack -> NewActivity*") {
+            $Status = $lgBeRightBack
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgDoNotDisturb *" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added DoNotDisturb*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: DoNotDisturb -> NewActivity*") {
+            $Status = $lgDoNotDisturb
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgFocusing*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added Focusing*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Focusing -> NewActivity*") {
+            $Status = $lgFocusing
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" } 
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgPresenting*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added Presenting*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Presenting -> NewActivity*") {
+            $Status = $lgPresenting
+            if ($debugEnabled -eq $true) {  Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgInAMeeting*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added InAMeeting*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: InAMeeting -> NewActivity*") {
+            $Status = $lgInAMeeting
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+        ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgOffline*" -or `
+                $TeamsStatus -like "*StatusIndicatorStateService: Added Offline*") {
+            $Status = $lgOffline
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+        }
+
+        If($TeamsActivity -eq $null){ }
+        ElseIf ($TeamsActivity -like "*Resuming daemon App updates*" -or `
+            $TeamsActivity -like "*SfB:TeamsNoCall*" -or `
+            $TeamsActivity -like "*name: desktop_call_state_change_send, isOngoing: false*") {
+            $Activity = $lgNotInACall
+            $ActivityIcon = $iconNotInACall
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Activity: $Activity" }
+        }
+        ElseIf ($TeamsActivity -like "*Pausing daemon App updates*" -or `
+            $TeamsActivity -like "*SfB:TeamsActiveCall*" -or `
+            $TeamsActivity -like "*name: desktop_call_state_change_send, isOngoing: true*") {
+            $Activity = $lgInACall
+            $ActivityIcon = $iconInACall
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Activity: $Activity" }
+        }
     }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgBusy*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added Busy*" -or `
-            $TeamsStatus -like "*Setting the taskbar overlay icon - $lgOnThePhone*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added OnThePhone*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Busy -> NewActivity*") {
-        $Status = $lgBusy
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgAway*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added Away*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Away -> NewActivity*") {
-        $Status = $lgAway
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgBeRightBack*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added BeRightBack*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: BeRightBack -> NewActivity*") {
-        $Status = $lgBeRightBack
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgDoNotDisturb *" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added DoNotDisturb*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: DoNotDisturb -> NewActivity*") {
-        $Status = $lgDoNotDisturb
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgFocusing*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added Focusing*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Focusing -> NewActivity*") {
-        $Status = $lgFocusing
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgPresenting*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added Presenting*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: Presenting -> NewActivity*") {
-        $Status = $lgPresenting
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgInAMeeting*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added InAMeeting*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added NewActivity (current state: InAMeeting -> NewActivity*") {
-        $Status = $lgInAMeeting
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-    }
-    ElseIf ($TeamsStatus -like "*Setting the taskbar overlay icon - $lgOffline*" -or `
-            $TeamsStatus -like "*StatusIndicatorStateService: Added Offline*") {
-        $Status = $lgOffline
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
+    # Set status to Offline when the Teams application is not running
+    Else {
+            $Status = $lgOffline
+            $Activity = $lgNotInACall
+            $ActivityIcon = $iconNotInACall
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Status: $Status" }
+            if ($debugEnabled -eq $true) { Write-Host "$(Get-Date) - Activity: $Activity" }
+            
     }
 
-    If($TeamsActivity -eq $null){ }
-    ElseIf ($TeamsActivity -like "*Resuming daemon App updates*" -or `
-        $TeamsActivity -like "*SfB:TeamsNoCall*" -or `
-        $TeamsActivity -like "*name: desktop_call_state_change_send, isOngoing: false*") {
-        $Activity = $lgNotInACall
-        $ActivityIcon = $iconNotInACall
-        # Write-Host "$(Get-Date) - Activity: $Activity"
-        # Add-Content $logFilePath "$(Get-Date) - Activity: $Activity"
+    # Call Home Assistant API to set the status and activity sensors
+    If ($CurrentStatus -ne $Status -and $Status -ne $null) {
+        $CurrentStatus = $Status
+        
+        if ($Activity -eq "In a call") {
+            $color = "red"
+        }
+
+        if ($Activity -eq "Not in a call") {
+            $color = "green"
+        }
+
+        if ($Status -eq "Do not disturb") {
+            $color = "red"
+        }
+
+        if ($Status -eq "Away") {
+            $color = "off"
+        }
+
+        if ($Status -eq "Available" -and $Activity -eq "Not in a call"){
+            $color = "green"
+        }
+
+        Invoke-PythonWebHook -color $color
+
     }
-    ElseIf ($TeamsActivity -like "*Pausing daemon App updates*" -or `
-        $TeamsActivity -like "*SfB:TeamsActiveCall*" -or `
-        $TeamsActivity -like "*name: desktop_call_state_change_send, isOngoing: true*") {
-        $Activity = $lgInACall
-        $ActivityIcon = $iconInACall
-        # Write-Host "$(Get-Date) - Activity: $Activity"
-        # Add-Content $logFilePath "$(Get-Date) - Activity: $Activity"
+
+
+    If ($CurrentActivity -ne $Activity) {
+
+        $CurrentActivity = $Activity
+
+        if ($Activity -eq "In a call") {
+            $color = "red"
+        }
+
+        if ($Activity -eq "Not in a call") {
+            $color = "green"
+        }
+        
+        if ($Status -eq "Do not disturb") {
+            $color = "red"
+        }
+
+        if ($Status -eq "Available" -and $Activity -eq "Not in a call"){
+            $color = "green"
+        }
+
+        if ($Status -eq "Away") {
+            $color = "off"
+        }
+
+        Invoke-PythonWebHook -color $color
     }
-}
-# Set status to Offline when the Teams application is not running
-Else {
-        $Status = $lgOffline
-        $Activity = $lgNotInACall
-        $ActivityIcon = $iconNotInACall
-        # Write-Host "$(Get-Date) - Status: $Status"
-        # Add-Content $logFilePath "$(Get-Date) - Status: $Status"
-        # Write-Host "$(Get-Date) - Activity: $Activity"
-        # Add-Content $logFilePath "$(Get-Date) - Activity: $Activity"
-}
-
-# Call Home Assistant API to set the status and activity sensors
-If ($CurrentStatus -ne $Status -and $Status -ne $null) {
-    $CurrentStatus = $Status
-	
-    if ($Activity -eq "In a call") {
-		$color = "red"
-	}
-
-    if ($Activity -eq "Not in a call") {
-		$color = "green"
-	}
-
-    if ($Status -eq "Do not disturb") {
-		$color = "red"
-	}
-
-    if ($Status -eq "Away") {
-		$color = "off"
-	}
-
-    if ($Status -eq "Available" -and $Activity -eq "Not in a call"){
-		$color = "green"
-	}
-
-    $params = @{
-     "color"="$color";
-     }
-	 
-    $params = $params | ConvertTo-Json
-    $result  = Invoke-RestMethod -Uri "http://192.168.1.34:8000/webhook" -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($params)) -ContentType "application/json" 
-    Write-EventLog -LogName "Application" -Source "TeamsStatus" -EventID 3001 -EntryType Information -Message "Teams Status called webhook, result: $result" -Category 1 -RawData 10,20
-    Write-Host "$(Get-Date) - Wehook result: $result"
-}
-
-
-If ($CurrentActivity -ne $Activity) {
-
-    $CurrentActivity = $Activity
-
-    if ($Activity -eq "In a call") {
-		$color = "red"
-	}
-
-    if ($Activity -eq "Not in a call") {
-		$color = "green"
-	}
-	
-    if ($Status -eq "Do not disturb") {
-		$color = "red"
-	}
-
-    if ($Status -eq "Available" -and $Activity -eq "Not in a call"){
-		$color = "green"
-	}
-
-    if ($Status -eq "Away") {
-		$color = "off"
-	}
-
-    $params = @{
-     "color"="$color";
-     }
-	 
-    $params = $params | ConvertTo-Json
-    $result = Invoke-RestMethod -Uri "http://192.168.1.34:8000/webhook" -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($params)) -ContentType "application/json" 
-    Write-EventLog -LogName "Application" -Source "TeamsStatus" -EventID 3001 -EntryType Information -Message "Teams Status called webhook, result: $result" -Category 1 -RawData 10,20
-    Write-Host "$(Get-Date) - Wehook result: $result"
-}
-    Start-Sleep 5
+    Start-Sleep $pollFrequency
 } Until ($Enable -eq 0)
